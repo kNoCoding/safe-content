25f607fb3c8b25956ce521b6f24e2c8fd3dd8fb8
Konstantin Nochevny <kosnobiz@gmail.com>
2023-11-18 19:12:49 +0200

    Create factory functions and utility functions

--- js/services/user-service.service.js
+++ js/services/user-service.service.js
@@ -1,29 +1,43 @@
 'use strict'
 
-var gUsers = [
-    { id: 'u101', username: 'puki', password: 'secret', lastLoginTime: 1601891998864, isAdmin: false, },
-    { id: 'u102', username: 'kuki', password: 'secret', lastLoginTime: 1601891998888, isAdmin: false, },
-]
+var gUsers
+var gIdNum = 100
 
 
 // returns users by the current sorting
-function getUsersToShow(){}
+function getUsersToShow() { }
 
 //the function should return the user object if found or null if not (HINT: use array.find) 
 // If the user successfully log-in, update his lastLoginDate 
 // Also save the loggedinUser to localStorage
-function doLogin(userName, password){}
+function doLogin(userName, password) { }
 
 // Private functions
 
 //will populate a users array with user objects (gUsers array with 3 users) 
-function _createUsers(){}
+function _createUsers() {
+    gUsers = loadFromStorage('usersDB')
+    if (gUsers && gUsers.length) return
+
+    gUsers = [_createUser('puki', 'secret', false), _createUser('muki', 'hahaha', false), _createUser('kuki', 'secret', true)]
+    _saveUsers()
+}
 
 // will create a single user object
-function _createUser(){}
+function _createUser(uname, psw, isAdmin) {
+    return {
+        id: makeId(),
+        uname,
+        psw,
+        lastLoginTime: getTimeStamp(),
+        isAdmin,
+    }
+}
 
 //that saves the users to localStorage
-function _saveUsers(){}
+function _saveUsers() {
+    saveToStorage('usersDB', gUsers)
+}
 
 
 
--- js/services/util.service.js
+++ js/services/util.service.js
@@ -1,28 +1,25 @@
 'use strict'
 
-function makeId(length = 6) {
-    var id = ''
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-    for (var i = 0; i < length; i++) {
-        id += possible.charAt(getRandomInt(0, possible.length))
-    }
+function makeId() {
+    var id = 'u' + ++gIdNum
     return id
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+    // The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
 
 function getTimeStamp() {
+    const currentDate = new Date()
+    const timestamp = currentDate.getTime()
+    return timestamp
+}
+
+function getDate() {
     var now = new Date();
-
-    // Padding with zero if the hour or minute is less than 10
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
-
-    return hours + ":" + minutes;
+    return now
 }
