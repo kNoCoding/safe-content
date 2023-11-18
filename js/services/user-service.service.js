19080eb692599d1225be3b1670bbeaeafe9e2c24
Konstantin Nochevny <kosnobiz@gmail.com>
2023-11-18 18:40:08 +0200

    Refactor to MVC

---
+++ js/services/user-service.service.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var gUsers = [
+    { id: 'u101', username: 'puki', password: 'secret', lastLoginTime: 1601891998864, isAdmin: false, },
+    { id: 'u102', username: 'kuki', password: 'secret', lastLoginTime: 1601891998888, isAdmin: false, },
+]
+
+
+// returns users by the current sorting
+function getUsersToShow(){}
+
+//the function should return the user object if found or null if not (HINT: use array.find) 
+// If the user successfully log-in, update his lastLoginDate 
+// Also save the loggedinUser to localStorage
+function doLogin(userName, password){}
+
+// Private functions
+
+//will populate a users array with user objects (gUsers array with 3 users) 
+function _createUsers(){}
+
+// will create a single user object
+function _createUser(){}
+
+//that saves the users to localStorage
+function _saveUsers(){}
+
+
+
